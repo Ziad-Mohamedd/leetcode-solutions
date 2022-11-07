43eee6eae3be9763af00a5b545bb06ef9a4c869d
Ziad Mohamed <zeyadmohamed613@gmail.com>
2022-11-07 11:35:24 +0200

    feat: 680

--- 680-Valid-Palindrome-II.ts
+++ 680-Valid-Palindrome-II.ts
@@ -1,30 +1,21 @@
 function validPalindrome(s: string): boolean {
-  const validString = s.replace(/[^0-9a-z]/gi, "").toLocaleLowerCase();
-  let left = 0;
-  let length = validString.length - 1;
-  let right = length;
-  let count = 0;
-  while (left <= Math.floor(length / 2)) {
-    if (validString[left] !== validString[right]) {
-        count++;
-      if (validString[left + 1] === validString[right]) {
-        left++;
-      }
-      if (validString[left] === validString[right - 1]) {
-        right--;
-      } else {
-        return false;
-      }
-    }
-    if(count > 1) {
-         return false
-    };
-    console.log(count);
-    left++;
-    right--;
+  let i = 0, j = s.length-1;
+  while (i < j) {
+      if (s[i] !== s[j]) {
+          return (isPalindrome2(s, i+1, j) || isPalindrome2(s, i, j-1));
   }
+      i++;
+      j--;
+  }
+  return true;
+};
 
+
+function isPalindrome2(s: string, i: number, j: number): boolean {
+  while (i < j) {
+      if (s[i] !== s[j]) return false
+      i++;
+      j--;
+  }
   return true;
 }
-
-console.log(validPalindrome('eeccccbebaeeabebccceea'));
